9fd6964e8bb68ec32155372a29d094e6183857d4
Kim Jinho <138093753+vlsrang@users.noreply.github.com>
2024-02-04 06:42:39 +0100

    Add files via upload

---
+++ 2번.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define x 4 
+int main(void)
+{
+	int i,j;
+	for(i=0;i<=x;i++)
+	{
+		for(j=x-i-1;j>0;j--)
+		{
+			printf("");
+		}
+		for(j=0;j<i;j++)
+		{
+			printf("£ª ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
